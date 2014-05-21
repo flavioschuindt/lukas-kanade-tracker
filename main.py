e51476b96a7081ef41d12fd5ae6de75027482e2c
Flávio Schuindt <flavio.schuindt@gmail.com>
2014-05-20 22:27:14 -0300

    - Reorganização de pastas - Adiciona testes com arquivos de bechmark do middlebury

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #coding: utf-8
 import sys
+import glob
+import os
 from math import sqrt, pow
 from time import time
 
@@ -12,6 +14,54 @@ from utils.utils import create_image_from_pixels, communicate_with_ffmpeg_by_pip
 						
 
 if __name__ == "__main__":
+	
+	path = os.path.join(sys.argv[1], "*.png")
+	frames = sorted(glob.glob(path))
+
+	total_frames = len(frames)
+	for i in range(total_frames):
+		# Open two frames
+		f1 = Image.open(frames[i])
+
+		next_frame = i+1
+		if next_frame == total_frames:
+			break
+		f2 = Image.open(frames[next_frame]) 
+
+		f1_levels = get_resolutions(f1, 3)
+		f2_levels = get_resolutions(f2, 3)
+		f1_levels.append(f1)
+		f2_levels.append(f2)
+
+
+		width, height = f1.size
+
+		# Temporal difference between frames
+		dt = diff(f1, f2)
+
+		# Apply sobel and harris to get the best points to track
+
+		start = time()
+		dx, dy = sobel(f1, 3)
+		print "Sobel: %.2f segundos" % (time() - start)
+
+		start = time()
+		corners = harris(dx, dy, width, height, 3)
+		print "Harris: %.2f segundos" % (time() - start)
+
+		# Obtain optical flow using pyramidal implemenatation of lukas kanade feature tracker
+		start = time()
+		optical_flow = lukas_kanade_pyramidal(corners, f1_levels, f2_levels, dx, dy, dt, 3)
+		for point, velocity_vector in optical_flow:
+			f1 = draw_velocity_vector(f1, point, velocity_vector)
+
+		print "Lukas Kanade Pyramidal: %.2f segundos" % (time() - start)
+		f1_name = os.path.basename(os.path.splitext(frames[i])[0])
+		f2_name = os.path.basename(os.path.splitext(frames[next_frame])[0])
+
+		f1.save("data/results/middlebury/army/flow_%s_to_%s.png" % (f1_name, f2_name), "png")
+
+'''if __name__ == "__main__":
 
 	pipe = communicate_with_ffmpeg_by_pipe(sys.argv[1], 'rgb24')
 
@@ -58,11 +108,8 @@ if __name__ == "__main__":
 		i += 1
 		f1 = f2
 
-	pipe.terminate()
+	pipe.terminate()'''
 
-	'''# Open two frames
-	f1 = Image.open(sys.argv[1])
-	f2 = Image.open(sys.argv[2])'''
 
 
 '''if __name__ == "__main__":
